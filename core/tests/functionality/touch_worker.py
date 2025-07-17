4b97d46ba93ef09a20da72207d91865c2e71e7cb
MingYuan-Chen <skydance1983@gmail.com>
2025-07-17 17:24:26 +0800

    update touch step to use tool at fixed path

--- core/tests/functionality/touch_worker.py
+++ core/tests/functionality/touch_worker.py
@@ -27,83 +27,24 @@ class TouchWorker(BaseTestWorker):
             TestStep(
                 command=commands[0],  
                 timeout=5,
-                description="Launch touch test tool on usb 1",
+                description="Launch touch test tool",
             ),
             TestStep(
-                command=commands[1],  
-                timeout=5,
-                description="Launch touch test tool on usb 2",
-            ),
-            TestStep(
-                command=commands[2],
+                command=commands[1],
                 timeout=5,
                 description="Touch the 9 red circles",
                 criteria="The 9 red circles should become green",
                 pre_condition="Please touch the 9 red circles on the screen",
-                post_check="Is the 9 red circles become green? \n NOTE: Please click 'Close' button after testing",
+                post_check="Is the 9 red circles become green? \n NOTE: Please click 'Close' button on Panel before you click PASS/FAIL button",
                 manual_only=True
             ),
             TestStep(
-                command=commands[3],  
+                command=commands[2],  
                 timeout=5,
-                description="Launch touch test tool",
+                description="Launch ts_test_mt",
             ),
             TestStep(
-                command=commands[4],
-                description="Single drag",
-                criteria="The cross mark should move on the screen when a single finger is dragged",
-                pre_condition="Please drag with a single finger on the screen",
-                post_check="Is the cross mark moving on the screen?",
-                manual_only=True
-            ),
-            TestStep(
-                command=commands[5],
-                timeout=5,
-                description="Double drag",
-                criteria="Two cross marks should move on the screen when two fingers are dragged",
-                pre_condition="Please drag with two fingers on the screen",
-                post_check="Are the two cross marks moving on the screen?",
-                manual_only=True
-            ),
-            TestStep(
-                command=commands[6],
-                timeout=5,
-                description="Drag the edge of the screen",
-                criteria="The cross mark should move along the edge of the screen",
-                pre_condition="Please drag along the edge of the screen",
-                post_check="Is the cross mark moving along the edge of the screen?",
-                manual_only=True
-            ),
-            TestStep(
-                command=commands[7],
-                timeout=5,
-                description="Drag the screen with water",
-                criteria="The cross mark should move on the screen even with water present",
-                pre_condition="Please drag the screen with water",
-                post_check="Is the cross mark moving on the screen?",
-                manual_only=True
-            ),
-            TestStep(
-                command=commands[8],
-                timeout=5,
-                description="Drag the screen by wet finger",
-                criteria="The cross mark should move on the screen when using a wet finger",
-                pre_condition="Please drag the screen with a wet finger",
-                post_check="Is the cross mark moving on the screen?",
-                manual_only=True
-            ),
-            TestStep(
-                command=commands[9],
-                timeout=5,
-                description="Drag the screen by finger with standard Hospital Grade Surgical Gloves",
-                criteria="The cross mark should move on the screen when using finger with surgical gloves",
-                pre_condition="Please drag the screen with finger wearing standard Hospital Grade Surgical Gloves",
-                post_check="Is the cross mark moving on the screen?",
-                manual_only=True
-            ),
-            TestStep(
-                command=commands[10],
-                timeout=5,
+                command=commands[3],
                 description="Single draw",
                 criteria="A line should be drawn on the screen when drawing with a single finger",
                 pre_condition="Please click the draw button and draw with a single finger on the screen",
@@ -111,7 +52,7 @@ class TouchWorker(BaseTestWorker):
                 manual_only=True
             ),
             TestStep(
-                command=commands[11],
+                command=commands[4],
                 timeout=5,
                 description="Double draw",
                 criteria="Two lines should be drawn on the screen when drawing with two fingers",
@@ -120,7 +61,43 @@ class TouchWorker(BaseTestWorker):
                 manual_only=True
             ),
             TestStep(
-                command=commands[12],
+                command=commands[5],
+                timeout=5,
+                description="Draw along the edge of the screen",
+                criteria="A line should be drawn along the edge of the screen",
+                pre_condition="Please click the draw button and draw along the edge of the screen",
+                post_check="Is the line drawn along the edge of the screen?",
+                manual_only=True
+            ),
+            TestStep(
+                command=commands[6],
+                timeout=5,
+                description="Draw with wet finger",
+                criteria="A line should be drawn on the screen even with wet finger",
+                pre_condition="Please click the draw button and draw with a wet finger on the screen",
+                post_check="Is the line drawn on the screen?",
+                manual_only=True
+            ),
+            TestStep(
+                command=commands[7],
+                timeout=5,
+                description="Draw with wet finger",
+                criteria="A line should be drawn on the screen even with wet finger",
+                pre_condition="Please click the draw button and draw with a wet finger on the screen",
+                post_check="Is the line drawn on the screen?",
+                manual_only=True
+            ),
+            TestStep(
+                command=commands[8],
+                timeout=5,
+                description="Draw the screen by finger with standard Hospital Grade Surgical Gloves",
+                criteria="A line should be drawn on the screen when using finger with surgical gloves",
+                pre_condition="Please click the draw button and draw with finger wearing standard Hospital Grade Surgical Gloves",
+                post_check="Is the line drawn on the screen?",
+                manual_only=True
+            ),
+            TestStep(
+                command=commands[9],
                 timeout=5,
                 description="Quit touch test tool",
                 pre_condition="Please click the quit button",
