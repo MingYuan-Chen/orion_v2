82e37559e93ff2a8337bb7eadd08e35d9393c53f
Dexter <dexter_chang@promate.com>
2025-11-17 16:55:49 +0800

    Display the Probe test item on Odin

--- gui/views/main_window.py
+++ gui/views/main_window.py
@@ -1069,7 +1069,7 @@ class MainWindowController(QObject):
         self.test_manager.register_test("functionality_power_button", "Power Button Test")
         self.test_manager.register_test("functionality_touch", "Touch Test")
         self.test_manager.register_test("functionality_usb", "USB Test")
-        if self.platform_name in ["Athena", "Argo", "Hydra", "Gemini"]:
+        if self.platform_name not in ["Athena", "Argo", "Hydra", "Gemini"]:
             self.test_manager.register_test("functionality_probe", "Probe Test")
         
         # set the log recorder
